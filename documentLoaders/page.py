449390fef9378b18ce8c1d26b4a37e1fac9d2bdc
Prince Singh <princesingh15122004@gmail.com>
2026-05-03 07:48:34 +0530

    removed render issue.

--- documentLoaders/page.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.document_loaders import WebBaseLoader
-url = "https://vibieprince.vercel.app"
-
-data = WebBaseLoader(url)
-
-docs = data.load()
-
-print(docs[0].page_content)
